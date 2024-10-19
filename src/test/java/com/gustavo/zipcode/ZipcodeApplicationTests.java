ae04a06b5cf7f22823d10b75ea5c05953b2a668a
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2024-10-19 10:55:01 -0300

    Initial commit

---
+++ src/test/java/com/gustavo/zipcode/ZipcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.zipcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
